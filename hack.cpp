a1ab2b30440fe982229f7654b3996fdff447125a
Adrian Hernando <ahsolanas@gmail.com>
2019-05-16 16:53:22 +0200

    Create hack.cpp

---
+++ hack.cpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <Nathalib.h>
+using namespace std;
+
+int main()
+{
+  While(True)
+  {    
+    cout << FindProcessByName("Calculator") << endl;
+    getchar();
+    cout << "-----------------------------------" << endl << endl;
+  }
+  return 0;
+}
